24eafee4832b79b7e738de8258e3c2fb807550f0
Sudhanshu singh <sudhanshu6073@gmail.com>
2025-11-29 01:09:30 +0530

    add notification feature

---
+++ Backend/src/controllers/notification.controller.js
@@ -0,0 +1,54 @@
+import { Notification } from "../models/notification.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const getUserNotifications = asyncHandler(async(req,res)=> {
+    const {page=1, limit=10} = req.query;
+    const skip = (page-1) * limit;
+
+    const notifications = await Notification.find({recipient: req.user._id})
+        .populate("sender", "username avatar")
+        .populate("recipe", "title")
+        .sort({createdAt: -1})
+        .skip(skip)
+        .limit(parseInt(limit));
+
+    const unreadCount = await Notification.countDocuments({
+        recipient: req.user._id,
+        isRead: false
+    })
+
+    return res.status(200).json(
+        new ApiResponse(200, {notifications, unreadCount}, "Notifications fetched succesfully")
+    )
+});
+
+
+const markNotificationRead = asyncHandler(async(req,res)=> {
+    const {notificationId} = req.params;
+
+    await Notification.findByIdAndUpdate(
+        notificationId,
+        {isRead: true}
+    );
+
+    return res.status(200).json(new ApiResponse(200, {}, "Marked as read"));
+});
+
+
+const markAllNotificationsRead = asyncHandler(async (req,res)=> {
+    await Notification.updateMany(
+        {recipient: req.user._id, isRead: false},
+        {$set: {isRead: true}}
+    );
+
+    return res.status(200).json(new ApiResponse(200, {}, "All marked as read"))
+});
+
+export {
+    getUserNotifications,
+    markNotificationRead,
+    markAllNotificationsRead
+
+}
---
+++ Backend/src/models/notification.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+    {
+        recipient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        type: {
+            type: String,
+            enum: ["LIKE_RECIPE", "LIKE_REVIEW", "COMMENT", "FOLLOW", "SYSTEM"],
+            required: true
+        },
+        recipe: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Recipe" // Optional: linked recipe
+        },
+        isRead: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {timestamps: true}
+)
+
+export const Notification = mongoose.model("Notification", notificationSchema);
