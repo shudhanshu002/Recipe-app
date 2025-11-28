e11462e1735cf68665c4da5a7e170da955808564
Sudhanshu singh <sudhanshu6073@gmail.com>
2025-11-29 01:13:57 +0530

    add security to authentication

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken';
-
+import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
@@ -20,3 +20,21 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         throw new ApiError(401, error?.message || "Invalid access token");
     }
 });
+
+export const optionalAuth = asyncHandler(async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+
+        if(!token) return next();
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select('-password -refreshToken');
+
+        if (user) {
+          req.user = user;
+        }
+        next();
+    } catch (error) {
+        next();
+    }
+})
