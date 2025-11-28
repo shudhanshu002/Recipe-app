2f7a37d7f5e0a3d3e7bb713b61fb4f5e042c4f72
Sudhanshu singh <sudhanshu6073@gmail.com>
2025-11-29 01:13:01 +0530

    add admin middleware

---
+++ Backend/src/middlewares/admin.middleware.js
@@ -0,0 +1,17 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+export const verifyAdmin = asyncHandler(async (req,res,next)=> {
+    if(!req.user) {
+        throw new ApiError(401, "Unauthorized");
+    }
+
+    const ADMIN_EMAILS = ['admin@pinchofyumclone.com', 'owner@recipe.com'];
+
+    
+    if (!ADMIN_EMAILS.includes(req.user.email)) {
+      throw new ApiError(403, 'Access Denied: Admins only');
+    }
+
+    next(); 
+})
