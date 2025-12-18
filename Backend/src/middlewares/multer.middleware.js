5c5987c399d4deee11f19d6cc1bccc080da1fa0c
Sudhanshu singh <sudhanshu6073@gmail.com>
2025-12-18 19:03:48 +0530

    multer middleware improve

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,18 +1,26 @@
 import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        const tempDir = './public/temp';
+
+        if (!fs.existsSync(tempDir)) {
+            fs.mkdirSync(tempDir, { recursive: true });
+        }
+
+        cb(null, tempDir);
     },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '_' + uniqueSuffix)
-    }
-})
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + '_' + uniqueSuffix + path.extname(file.originalname));
+    },
+});
 
 export const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 5 * 1024 * 1024 * 1024 // 5GB
-    }
-})
+        fileSize: 5 * 1024 * 1024 * 1024, // 5GB
+    },
+});
